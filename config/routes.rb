adc91a12a3b468ccd407bf107a884b6ee72af2e4
LauraUmana <laurau1107@gmail.com>
2016-02-10 17:41:34 +0000

    Add drag and drop feature to lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Flixter::Application.routes.draw do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
-  namespace :instructor do 
+  namespace :instructor do
+    resources :lessons, only: [:update] 
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
